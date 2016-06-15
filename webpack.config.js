789e35c5ef4af93482c2e28b303b52a711bf1706
Jarmo Isotalo <jamo@isotalo.fi>
2016-06-15 16:40:22 +0300

    TmcAnalytics

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 var path = require('path');
 var env = require('yargs').argv.mode;
 
-var libraryName = 'tmc';
+var libraryName = 'TmcAnalytics';
 
 var plugins = [], outputFile;
 
