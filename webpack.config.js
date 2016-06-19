a4a650e7f1425a2028318e6d5136832051ea6102
Jarmo Isotalo <jamo@isotalo.fi>
2016-06-19 16:01:38 +0300

    Update to webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,31 @@
-var webpack = require('webpack');
-var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
-var path = require('path');
-var env = require('yargs').argv.mode;
+const webpack = require('webpack')
+const path = require('path')
+const resolve = path.resolve
 
-var libraryName = 'TmcAnalytics';
 
-var plugins = [], outputFile;
-
-if (env === 'build') {
-  plugins.push(new UglifyJsPlugin({ minimize: true }));
-  outputFileExtension = '.min.js';
-} else {
-  outputFileExtension = '.js';
+module.exports = env => {
+  return {
+    entry: './src/index.js',
+    output: {
+      path: resolve('.'),
+      filename: 'tmc-analytics' + (env.prod ? '.min.js' : '.js'),
+      library: 'TmcAnalytics' ,
+      libraryTarget: 'umd'
+    },
+    module: {
+      loaders: [
+      { test: /(\.jsx|\.js)$/, loader: 'babel!eslint', exclude: /(node_modules|bower_components)/ },
+      { test: /(\.jsx|\.js)$/, loader: "eslint-loader", exclude: /node_modules/ }
+      ]
+    },
+    resolve: {
+      root: path.resolve('./src'),
+      extensions: ['', '.js']
+    },
+    plugins: [
+      env.prod ? undefined : webpack.optimize.UglifyJsPlugin({minimize: true})
+    ].filter(p => !!p),
+    devtool: 'source-map',
+  }
 }
 
-var config = {
-  entry: __dirname + '/src/index.js',
-  devtool: 'source-map',
-  output: {
-    path: __dirname + '/',
-    filename: 'tmc-analytics' + outputFileExtension,
-    library: libraryName ,
-    libraryTarget: 'umd'
-  },
-  module: {
-    loaders: [
-      {
-        test: /(\.jsx|\.js)$/,
-        loader: 'babel',
-        exclude: /(node_modules|bower_components)/,
-        query: {
-          presets: ['es2015'],
-          plugins: ['transform-runtime']
-        }
-      },
-      {
-        test: /(\.jsx|\.js)$/,
-        loader: "eslint-loader",
-        exclude: /node_modules/
-      }
-    ]
-  },
-  resolve: {
-    root: path.resolve('./src'),
-    extensions: ['', '.js']
-  },
-  plugins: plugins
-};
-
-module.exports = config;
